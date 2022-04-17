eb8f694ed7d50cb0ee5f53e25c579aa35475f41d
demps123 <92432723+demps123@users.noreply.github.com>
2022-04-17 20:22:33 +0100

    test

---
+++ My project/Assets/Scripts/Examples/CarAITargetMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAITargetMouse : MonoBehaviour {
+
+    [SerializeField] private Transform targetTransform;
+
+    private bool isFollowing = false;
+
+    private void Update() {
+        if (isFollowing) {
+            targetTransform.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            isFollowing = !isFollowing;
+        }
+    }
+
+}
