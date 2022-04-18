3e9ab1337fc38270e92e52218f3d416943808a37
demps123 <92432723+demps123@users.noreply.github.com>
2022-04-18 20:52:46 +0100

    Installed ML Agents and applied scripts to Player Object

--- My project/Assets/Scripts/TrackCheckpoint.cs
+++ My project/Assets/Scripts/TrackCheckpoint.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackCheckpoint : MonoBehaviour
-{
-    private List<CheckpointSingle> checkpointSingleList;
+public class TrackCheckpoint : MonoBehaviour {
 
-    private void Awake()
-    {
+    private List<CheckpointSingle> checkpointSingleList;
+    private int nextCheckpointSingleIndex;
+
+    private void Awake() {
         Transform checkpointsTransform = transform.Find("Checkpoints");
 
         checkpointSingleList = new List<CheckpointSingle>();
-
-        foreach (Transform checkpointSingleTransform in checkpointsTransform)
-        {
+        foreach (Transform checkpointSingleTransform in checkpointsTransform) {
             CheckpointSingle checkpointSingle = checkpointSingleTransform.GetComponent<CheckpointSingle>();
+
             checkpointSingle.SetTrackCheckpoints(this);
             checkpointSingleList.Add(checkpointSingle);
         }
+        nextCheckpointSingleIndex = 0;
     }
 
-    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle)
-    {
-        Debug.Log(checkpointSingleList.IndexOf(checkpointSingle));
+    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle) {
+        if (checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex) {
+            Debug.Log("Correct");
+            nextCheckpointSingleIndex = (nextCheckpointSingleIndex + 1) % checkpointSingleList.Count;
+        }
+        else {
+            Debug.Log("Incorrect");
+        }
     }
 }
